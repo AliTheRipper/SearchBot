d8c7f027f2d0066259512967891914de78351efa
ALI_ZIFOUTI <alizifouti@gmail.com>
2025-04-11 13:37:55 +0200

    spinner dans login

--- SearchBot/front/src/views/login.vue
+++ SearchBot/front/src/views/login.vue
@@ -7,8 +7,14 @@
 
       <input v-model="password" type="password" placeholder="Mot de passe" required />
       <p class="error" v-if="validationErrors.password">{{ validationErrors.password[0] }}</p>
+      
+      
+      <div v-if="loading" class="spinner"></div>
+      <button type="submit" :disabled="loading">
+        <span v-if="!loading">Login</span>
+        <span v-else>Logging in...</span>
+      </button>
 
-      <button type="submit">Se connecter</button>
     </form>
     <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
     <router-link to="/register">Pas encore inscrit ?</router-link>
@@ -26,10 +32,13 @@ const email = ref('')
 const password = ref('')
 const errorMessage = ref('')
 const validationErrors = ref({})
-
 const router = useRouter()
+const loading = ref(false)
+
 
 const login = async () => {
+  loading.value = true
+
   try {
   const response = await axios.post('http://127.0.0.1:8000/api/login', {
     email: email.value,
@@ -41,6 +50,8 @@ const login = async () => {
     router.push('/home')
   } else {
     errorMessage.value = response.data.message || "Identifiants incorrects"
+    loading.value = false
+
   }
 } catch (error) {
   if (error.response?.status === 422) {
@@ -52,6 +63,9 @@ const login = async () => {
 
   console.error(error.response?.data || error)
 }
+finally {
+    loading.value = false
+  }
 }
 </script>
 
@@ -78,5 +92,18 @@ cursor: pointer;
 }
 .error {
 color: red;
+}
+.spinner {
+  margin: 10px auto;
+  border: 4px solid rgba(255, 255, 255, 0.3);
+  border-top: 4px solid #fff;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  to { transform: rotate(360deg); }
 }
 </style>
